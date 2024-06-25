8744885f0c348818a10cff663608879433b70e24
fortunatus <ihedfortune@gmail.com>
2024-06-25 12:25:06 +0100

    consume middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReverseProxyAuthMiddleware } from './middleware/proxy-auth.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer:MiddlewareConsumer) {
+    consumer.apply(ReverseProxyAuthMiddleware).forRoutes({path: 'v1/auth-service/*', method: RequestMethod.ALL})
+  }
+}
