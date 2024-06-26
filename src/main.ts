7a1f6292d93a79bb26d4aa2883148127687d4124
fortunatus <ihedfortune@gmail.com>
2024-06-26 13:25:16 +0100

    globalPrefix

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {bodyParser:false});
 
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
 
-  await app.listen(3000);
+  app.use((req, res, next) => {
+    console.log(`GOT INVOKED: ${req.originalUrl}`);
+    next();
+  });
+
+  await app.listen(process.env.PROXY_PORT || 3001, () => {
+    console.log(`listening at ${process.env.PROXY_PORT}/${globalPrefix}`);
+    
+  });
 }
 bootstrap();
-`
